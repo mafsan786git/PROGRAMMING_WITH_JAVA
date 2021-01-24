098beac13ac94fbe4554a542b4df0afd4d1d02a2
mafsan786git <mafsan78692@gmail.com>
2021-01-25 02:51:16 +0530

    cses dp problem added

--- CSES_PROBLEM/IncreasingSubSeq.java
+++ CSES_PROBLEM/IncreasingSubSeq.java
@@ -1,7 +1,23 @@
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class IncreasingSubSeq {
+    public static int UpperBound(int[] arr,int right,int key){
+        int left = 0;
+        int ans=0;
+        while(left <= right)
+        {
+            int mid = left + (right-left)/2;
+            if(key<= arr[mid])
+            {
+                ans = mid;
+                right = mid-1;
+            }else
+                left = mid+1;
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         int n = sc.nextInt();
@@ -10,17 +26,18 @@ public class IncreasingSubSeq {
             input[i] = sc.nextInt();
         }
         int[] dp = new int[n];
-        Arrays.fill(dp,0,n,1);
+        dp[0] = input[0];
+        int lenMax = 1;
         for (int i = 1; i < dp.length; i++) {
-            for (int j = 0; j < i; j++) {
-                dp[i] = (input[i] > input[j]) ? Math.max(dp[j]+1,dp[i]) : dp[i];
-            }
+            if(input[i] < dp[0])
+                dp[0] = input[i];
+            else if(input[i] > dp[lenMax-1])
+                dp[lenMax++] = input[i];
+            else
+                dp[UpperBound(dp,lenMax-1, input[i])] = input[i];
+            // System.out.println(Arrays.toString(dp));
         }
-        int ans  = 0;
-        for(Integer val : dp){
-            ans = (val > ans) ? val  : ans;
-        }
-        System.out.println(ans);
+        System.out.println(lenMax);
         sc.close();
     }
     
