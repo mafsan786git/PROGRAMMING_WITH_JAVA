4f1e6f0baa0c5b455ba3278c4e3381a0bb6895ff
Mohd. Afsan Ahmad <mafsan78692@gmail.com>
2023-08-07 22:26:56 +0530

    added pascal triangle

---
+++ LeetCode/Pascals118.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pascals118 {
+    public static void main(String[] args) {
+        List<List<Integer>> answer = new ArrayList<>(10);
+        answer.add(new ArrayList<>(List.of(1)));
+    }
+}
