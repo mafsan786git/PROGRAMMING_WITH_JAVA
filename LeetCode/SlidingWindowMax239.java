dd67d27b558e4766bb5c47abe1ae1912cec6d6c3
Mohd. Afsan Ahmad <41589765+mafsan786git@users.noreply.github.com>
2023-09-24 09:41:53 +0000

    added some changes in slidingWindow

--- LeetCode/SlidingWindowMax239.java
+++ LeetCode/SlidingWindowMax239.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public class SlidingWindowMax239 {
     public static void main(String[] args) {
-        int[] answer = maxValuesOptimsed(new int[]{1,3,-1,-3,5,3,6,7}, 3);
+        int[] answer = maxValuesOptimsed(new int[]{1,3,-1,-3,5,3,6,7}, 2);
         System.out.println(Arrays.toString(answer));
     }
 
