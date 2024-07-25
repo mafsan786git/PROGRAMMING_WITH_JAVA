fe855edc09f90298887c144b761646614fb7f40b
Mohd. Afsan Ahmad <mafsan78692@gmail.com>
2024-07-25 11:00:19 +0530

    added duplicate zeros

--- LeetCode/DuplicateZeros1089.java
+++ LeetCode/DuplicateZeros1089.java
@@ -2,8 +2,8 @@
 import java.util.*;
 public class DuplicateZeros1089 {
     public static void main(String[] arg){
-        int[] arr = new int[]{1,0,2,3,0,4,5,0};
-        duplicateZeros(arr);
+        int[] arr = new int[]{8,4,5,0,0,0,0,7};
+        duplicateZerosOptimised_inplace(arr);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -19,5 +19,46 @@ public class DuplicateZeros1089 {
                 index++;
             }
         }
+    }
+
+    public static void duplicateZerosOptimised(int[] arr){
+        Queue<Integer> queue = new LinkedList<>();
+        for (int num : arr) {
+            queue.add(num);
+        }
+        for(int index = 0;index < arr.length;index++){
+            int firstElement = queue.poll();
+            arr[index] = firstElement;
+            if(firstElement == 0){
+                index++;
+                if(index < arr.length){
+                    arr[index] = 0;
+                }
+            }
+        }
+    }
+
+    public static void duplicateZerosOptimised_inplace(int[] arr){
+        int trimEndLength = 0;
+        int length = arr.length-1;
+        for(int index = 0;index <= length-trimEndLength;index++){
+            if(arr[index] == 0){
+                if(index == length-trimEndLength){
+                    arr[length] = 0;
+                    length--;
+                    break;
+                }
+                trimEndLength++;
+            }
+        }
+        int lastIndex = length-trimEndLength;
+        for(int index = lastIndex;index >=0; index--){
+            if(arr[index] == 0){
+                arr[index+trimEndLength] = 0;
+                trimEndLength--;
+            }
+            arr[index+trimEndLength] = arr[index];
+        }
+        System.out.println(trimEndLength);
     }
 }
